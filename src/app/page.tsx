b8f8fb3ed769e89910349f5a7f9ebdf20501753c
alfian <devsroot8@gmail.com>
2023-12-22 11:00:24 +0700

    say hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex justify-center items-center">
-      <h1 className="font-medium text-xl text-white">Hello there, I'am Alfian 👋</h1>
+      <h1 className="font-medium text-xl text-white">Hello there, Im Alfian 👋</h1>
       <h1></h1>
     </main>
   )
