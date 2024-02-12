e1f48491ee0388814f8b6e022d703196fefa8237
alfian <devsroot8@gmail.com>
2024-02-12 13:06:32 +0700

    feat: add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Al Fiansyah — Software Engineer Frontend',
-  description: 'gus jabung al fiansyah portofolio',
+  title: 'Gus Jabung Al Fiansyah — Software Engineer Frontend',
+  description: 'Gus Jabung Al Fiansyah Portofolio',
 }
 
 export default function RootLayout({
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-zinc-950`}>{children}</body>
+      <body className={`${inter.className} bg-zinc-950`}>
+          <Header/>
+          {children}
+        </body>
     </html>
   )
 }
