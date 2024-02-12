4fe95f2694c5b3521891ddf1acf8a799c2d133c2
alfian <devsroot8@gmail.com>
2024-02-12 13:01:23 +0700

    add static data

---
+++ src/static/index.ts
@@ -0,0 +1,14 @@
+export const NavLink = [
+    {
+        label: 'home',
+        link: '/'
+    },
+    {
+        label: 'work',
+        link: '/work'
+    },
+    {
+        label: 'contact',
+        link: '/contact'
+    }
+]
