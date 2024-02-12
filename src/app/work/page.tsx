827e3e42880a46b847d7ba50126a82fcca7273bf
alfian <devsroot8@gmail.com>
2024-02-12 13:00:36 +0700

    feat: add work page

---
+++ src/app/work/page.tsx
@@ -0,0 +1,8 @@
+const Work = () => {
+    return <main className="w-full min-h-screen flex flex-col items-center justify-center">
+                <h1 className="text-white">Stay Tune !</h1>
+                <h1 className="text-white">this page will available soon ✨</h1>
+            </main>
+}
+
+export default Work
