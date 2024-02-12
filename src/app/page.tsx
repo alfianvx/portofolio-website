4191c5e806adc6e2656b1816097dfdcae2672f63
alfian <devsroot8@gmail.com>
2024-02-12 13:06:54 +0700

    change content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex justify-center items-center">
-      <h1 className="font-medium text-xl text-white">Hello there, Im Alfian 👋</h1>
-      <h1></h1>
+    <main className="max-w-2xl mx-auto min-h-screen mt-24 px-2 sm:px-0">
+      <section className="flex flex-col sm:flex-row sm:items-end space-x-0 space-y-4 sm:space-x-5 sm:space-y-0 mb-3 sm:mb-7">
+        <Image alt="gus jabung al fiansyah - photo" src='/alfian-profile.jpg' width={200} height={450} className="rounded-md"/>
+        <div>
+        <h1 className="text-white text-2xl sm:text-4xl font-bold mb-1">Gus Jabung Al Fiansyah</h1>
+        <h3 className="text-white text-xs sm:text-md tracking-normal mb-2">a <span className="font-semibold">Software Engineer Frontend</span></h3>
+        </div>
+      </section>
+      <article>
+      <p className="text-white text-sm sm:text-md mb-3 sm:mb-5 tracking-wide">
+      I'm a Software Engineer Frontend specializing in web development technologies, using popular tools like <span className="font-bold">TypeScript</span> for programming, <span className="font-bold">NextJS</span> for the framework, and <span className="font-bold">TailwindCSS</span> for styling.
+      </p>
+      <p className="text-white text-sm sm:text-md mb-3 sm:mb-5 tracking-wide">
+       I possess a fervent enthusiasm for designing User Interfaces across Web, Desktop, and Mobile platforms, with a particular proficiency in utilizing tools such as <span className="font-bold">Figma</span>.
+      </p>
+      </article>
     </main>
   )
 }
