e8372efe9e78591d32f66fd2c9aedf9f2e614887
alfian <devsroot8@gmail.com>
2024-02-13 15:45:59 +0700

    feat: add border when scroll down

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,23 @@
+"use client";
+
 import Link from "next/link";
 import { NavLink } from "@/static";
+import { useWindowYAxis } from "@/hooks/use-win-y-axis";
 import ThemeToggle from "@/components/theme-toggle";
 
 const Header = () => {
+  const y = useWindowYAxis();
+
   return (
-    <header className="w-full fixed top-0 px-4 sm:px-0">
+    <header
+      className={`w-full fixed top-0 px-4 md:px-0 bg-[#fff] dark:bg-[#09090B] ${
+        y > 20 && "border-b"
+      }`}
+    >
       <div className="max-w-2xl flex items-center justify-between mx-auto py-4">
         <nav className="flex space-x-3">
           {NavLink.map(({ label, link }) => (
-            <Link key={label} href={link}>
+            <Link key={label} href={link} className="font-medium">
               {label}
             </Link>
           ))}
