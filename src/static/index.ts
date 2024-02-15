4a657e11addbe7f6a4f0dc114011962e02600d68
alfian <devsroot8@gmail.com>
2024-02-15 08:58:08 +0700

    add static data for project and about link for navbar

--- src/static/index.ts
+++ src/static/index.ts
@@ -4,11 +4,67 @@ export const NavLink = [
     link: "/",
   },
   {
+    label: "about",
+    link: "/about",
+  },
+  {
     label: "work",
     link: "/work",
   },
   {
     label: "contact",
     link: "/contact",
+  },
+];
+
+export const MyProjects = [
+  {
+    title: "Body Mass Idex Calculator",
+    link: "https://js-bmi-calculator.vercel.app/",
+    description: "Put your Heigh and Wight to see your BMI Health!",
+    tech: ["HTML", "Bootstrap", "JS"],
+    repo: "",
+    image: "/works/BMI.png",
+  },
+  {
+    title: "React Todos",
+    link: "https://react-thodos.vercel.app/",
+    description: "Track activity for productive day",
+    tech: ["React", "TailwindCSS", "Redux"],
+    repo: "",
+    image: "/works/Todo.png",
+  },
+  {
+    title: "Kulineri",
+    link: "https://kulineri-frontend.vercel.app/",
+    description:
+      "MSIB - Challange | Kuliner - Taste the Archipelago: Kulineri, Kunci Kelezatan Nusantara !",
+    tech: ["React", "TailwindCSS", "Redux", "ExpressJS", "MongoDB"],
+    repo: "",
+    image: "/works/Kulineri.png",
+  },
+  {
+    title: "NextJS Dashboard",
+    link: "https://nextjs-dashboard-rosy-nu-29.vercel.app",
+    description: "Record income and print monthly report company",
+    tech: ["NextJS", "PostgreSQL", "TailwindCSS", "Vercel Postgre", "JWT"],
+    repo: "",
+    image: "/works/Dashboard.png",
+  },
+  {
+    title: "DeFi Pie Landing Page",
+    link: "https://defipie-landing-page.vercel.app/",
+    description: "Slicing figma design into code",
+    tech: ["NextJS", "TailwindCSS", "TypeScript"],
+    repo: "",
+    image: "/works/DeFi.png",
+  },
+  {
+    title: "Portovolio V1",
+    link: "https://alfiandev.vercel.app/",
+    description: "My portofolio website create with Love",
+    tech: ["HTML", "CSS", "JS"],
+    repo: "",
+    image: "/works/Portofolio.png",
   },
 ];
