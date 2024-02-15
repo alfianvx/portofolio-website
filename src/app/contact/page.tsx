111b4a346feef7f2b538a29b6b36ce859a119ea1
alfian <devsroot8@gmail.com>
2024-02-15 08:58:49 +0700

    add content & links

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,34 @@
+import Link from "next/link";
+
 const Contact = () => {
   return (
-    <main className="w-full min-h-screen flex flex-col items-center justify-center">
-      <h1>Stay Tune !</h1>
-      <h1>this page will available soon ✨</h1>
+    <main className="max-w-2xl mx-auto min-h-screen py-32 px-4 md:px-0">
+      <h1 className="text-2xl font-medium">Contact</h1>
+      <p className="mb-5">
+        No need to shy, get in touch with me with link bellow:
+      </p>
+      <ul className="space-y-2 list-disc pl-4">
+        <li>
+          <Link href={""} className="hover:underline">
+            Github
+          </Link>
+        </li>
+        <li>
+          <Link href={""} className="hover:underline">
+            Telegram
+          </Link>
+        </li>
+        <li>
+          <Link href={""} className="hover:underline">
+            Linkedin
+          </Link>
+        </li>
+        <li>
+          <Link href={""} className="hover:underline">
+            Email
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 };
